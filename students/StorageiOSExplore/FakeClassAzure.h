b0d44551cedf7d6ba32414c55f84899cc20b6238
amdaza <amdazacastillo@gmail.com>
2016-10-23 18:15:50 +0200

    Created project & initial files

---
+++ students/StorageiOSExplore/FakeClassAzure.h
@@ -0,0 +1,13 @@
+//
+//  FakeClassAzure.h
+//  StorageiOSExplore
+//
+//  Created by Home on 23/10/16.
+//  Copyright © 2016 Alicia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FakeClassAzure : NSObject
+
+@end
---
+++ students/StorageiOSExplore/FakeClassAzure.m
@@ -0,0 +1,13 @@
+//
+//  FakeClassAzure.m
+//  StorageiOSExplore
+//
+//  Created by Home on 23/10/16.
+//  Copyright © 2016 Alicia. All rights reserved.
+//
+
+#import "FakeClassAzure.h"
+
+@implementation FakeClassAzure
+
+@end
