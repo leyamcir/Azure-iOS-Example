aff3a25c061ad44d46acbf5711f25952f02c8808
amdaza <amdazacastillo@gmail.com>
2016-10-23 21:23:30 +0200

    Container table with segue navigation

--- students/StorageiOSExplore/StorageiOSExplore/ContainersTableViewController.swift
+++ students/StorageiOSExplore/StorageiOSExplore/ContainersTableViewController.swift
@@ -188,30 +188,29 @@ class ContainersTableViewController: UITableViewController {
         }    
     }
     
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let item = self.model[indexPath.row]
+        
+        performSegue(withIdentifier: "selectContainer", sender: item)
     }
-    */
 
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "selectContainer" {
+            
+            let vc = segue.destination as! ContainerTableViewController
+            vc.client = client
+            vc.container = sender as? AZSCloudBlobContainer
+        }
     }
-    */
+    
 
 }
